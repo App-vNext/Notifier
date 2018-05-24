6b9947778b57c55086f07a53201948db214124f5
Gerardo Melendrez <payini@hotmail.com>
2018-05-23 23:18:25 -0700

    Added Errors, Register and Duration properties.

--- src/AppVNext.Notifier/AppVNext.Notifier/NotificationArguments.cs
+++ src/AppVNext.Notifier/AppVNext.Notifier/NotificationArguments.cs
@@ -10,16 +10,20 @@ namespace AppVNext.Notifier
 		public string Title { get; internal set; }
 		public string Message { get; internal set; }
 		public string PicturePath { get; internal set; }
-
 		public string WindowsSound { get; internal set; }
 		public string Loop { get; internal set; } = "false";
 		public string SoundPath { get; internal set; }
 		public bool Silent { get; internal set; }
 		public bool Wait { get; internal set; }
+		public string Errors { get; internal set; }
+		public bool Register { get; internal set; }
+		public string Duration { get; internal set; }
 
 		internal bool AreValid()
 		{
-			return !String.IsNullOrWhiteSpace(Message);
+			var isValid = string.IsNullOrWhiteSpace(Errors) 
+				&& (string.IsNullOrWhiteSpace(Duration) || !string.IsNullOrWhiteSpace(Duration));
+			return isValid; 
 		}
 	}
 }

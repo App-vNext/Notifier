0db6957727bfbcee664e83c5904427389d8b59b2
Gerardo Melendrez <payini@hotmail.com>
2018-05-23 23:17:53 -0700

    Added duration help. Updated text.

--- src/AppVNext.Notifier/AppVNext.Notifier/Globals.cs
+++ src/AppVNext.Notifier/AppVNext.Notifier/Globals.cs
@@ -16,44 +16,60 @@ namespace AppVNext.Notifier
 
 		//Arguments help text
 		internal static readonly string HelpForRegister =
-			$"{NewLine}Argument -r requires 2 values: <appId string> and <appName string>.{NewLine}" +
-			$"Example: -r \"com.brave.windows-notifier\" \"brave-windows-notifier\"";
+			$"Argument -r requires 2 values: <appId string> and <appName string>.{NewLine}" +
+			$"Example: -r \"com.brave.windows-notifier\" \"brave-windows-notifier\"{NewLine}";
 
 		internal static readonly string HelpForTitle =
-			$"{NewLine}Argument -t requires 1 value: <title string>.{NewLine}" +
-			$"Example: -t \"Notification Title\"";
+			$"Argument -t requires 1 value: <title string>.{NewLine}" +
+			$"Example: -t \"Notification Title\"{NewLine}";
 
 		internal static readonly string HelpForMessage =
-			$"{NewLine}Argument -m requires 1 value: <message string>.{NewLine}" +
-			$"Example: -m \"Notification message.\"";
+			$"Argument -m requires 1 value: <message string>.{NewLine}" +
+			$"Example: -m \"Notification message.\"{NewLine}";
 
 		internal static readonly string HelpForPicture =
-			$"{NewLine}Argument -p requires 1 value: <image URI>.{NewLine}" +
-			$"Example: -p \"C:\\Pictures\\Picture.png\"";
+			$"Argument -p requires 1 value: <image URI>.{NewLine}" +
+			$"Example: -p \"C:\\Pictures\\Picture.png\"{NewLine}";
 
 		internal static readonly string HelpForId =
-			$"{NewLine}Argument -id requires 1 value: <id string>.{NewLine}" +
-			$"Example: -id \"12345\"";
+			$"Argument -id requires 1 value: <id string>.{NewLine}" +
+			$"Example: -id \"12345\"{NewLine}";
 
 		internal static readonly string HelpForSound =
-			$"{NewLine}Argument -s requires 1 value: <title string>.{NewLine}" +
-			$"Example: -s \"C:\\Sounds\\Sound.wav\"";
+			$"Argument -s requires 1 value: <title string>.{NewLine}" +
+			$"Example: -s \"C:\\Sounds\\Sound.wav\"{NewLine}";
 
 		internal static readonly string HelpForAppId =
-			$"{NewLine}Argument -appID requires 1 value: <appID string>.{NewLine}" +
-			$"Example: -appID \"com.brave.windows-notifier\"";
+			$"Argument -appID requires 1 value: <appID string>.{NewLine}" +
+			$"Example: -appID \"com.brave.windows-notifier\"{NewLine}";
 
 		internal static readonly string HelpForClose =
-			$"{NewLine}Argument -close requires 1 value: <ID string>.{NewLine}" +
-			$"Example: -close \"12345\"";
+			$"Argument -close requires 1 value: <ID string>.{NewLine}" +
+			$"Example: -close \"12345\"{NewLine}";
+
+		internal static readonly string HelpForDuration =
+			$"Argument -d requires 1 value: <short|long>.{NewLine}" +
+			$"Example: -d \"long\"{NewLine}";
 
 		internal static readonly string HelpForInvalidArgument =
-			$"{NewLine}Argument {{0}} is invalid.{NewLine}" +
-			$"Please type notifier help to show the valid arguments.";
+			$"Argument '{{0}}' is invalid.{NewLine}";
+
+		internal static readonly string HelpForNullMessage =
+			$"At least argument -m <message string> is required.{NewLine}" +
+			$"Example: -m \"Notification message.\"{NewLine}";
+
+		internal static readonly string HelpForErrors = 
+			$"{NewLine}Please type 'notifier help' to show the valid arguments.{NewLine}";
+
+		internal static readonly string HelpForRegisterSuccess =
+			$"The application shortcut for '{{0}}' '{{1}}' was successfully created.{NewLine}";
+
+		internal static readonly string HelpForRegisterFail =
+			$"The application shortcut for '{{0}}' '{{1}}' already existed.{NewLine}";
 
 		//Help text
 		internal static readonly string HelpText =
-			$"{NewLine}Create a send notifications.{NewLine}{NewLine}" +
+			$"Create a send notifications.{NewLine}{NewLine}" +
 			$"Usage: notifier <command>{NewLine}{NewLine}" +
 			$"Commands:{NewLine}{NewLine}" +
 			$"[-r] <appId string><appName string>	Registers notifier into the Windows machine.{NewLine}" +
@@ -65,6 +81,7 @@ namespace AppVNext.Notifier
 			$"[s] [<sound URI>][<Windows sound>]	URI for a sound file or Windows sound to play when the notification displays.{NewLine}" +
 			$"					For possible Windows sound values visit http://msdn.microsoft.com/en-us/library/windows/apps/hh761492.aspx.{NewLine}" +
 			$"[-silent]				Does not play a sound when showing the notification.{NewLine}" +
+			$"[-d] <short|long>				Determines how long to display the notification for. Default is 'short'.{NewLine}" +
 			$"[-appID] <appID string>			Used to display the notification.{NewLine}" +
 			$"[-close] <ID string>			Closes notification. In order to be able to close a notification,{NewLine}" +
 			$"					the parameter -w must be used to create the notification.{NewLine}" +

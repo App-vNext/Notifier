00fccc39b4049d44cb0be19952cbe537095e128f
Gerardo Melendrez <payini@hotmail.com>
2018-05-23 23:18:48 -0700

    Added Exit codes.

--- src/AppVNext.Notifier/AppVNext.Notifier/NotificationEvents.cs
+++ src/AppVNext.Notifier/AppVNext.Notifier/NotificationEvents.cs
@@ -1,5 +1,7 @@
 ﻿using System;
 using Windows.UI.Notifications;
+using static System.Environment;
+using static System.Console;
 
 namespace AppVNext.Notifier
 {
@@ -7,31 +9,42 @@ namespace AppVNext.Notifier
 	{
 		internal void Activated(ToastNotification sender, object e)
 		{
-			Console.WriteLine("The user activated the notification");
+			WriteLine("The user activated the notification");
+			Exit(0);
 		}
 
 		internal void Dismissed(ToastNotification sender, ToastDismissedEventArgs e)
 		{
-			String outputText = string.Empty;
 			switch (e.Reason)
 			{
+				//Failed	-1
+				//Success	0
+				//Hidden	1
+				//Dismissed	2
+				//Timeout	3
+
 				case ToastDismissalReason.ApplicationHidden:
-					outputText = "The app hid the notification using ToastNotifier.Hide.";
+					//					var d = DismissalActions.Hidden;
+					WriteLine("The app hid the notification using ToastNotifier.Hide.");
+					Exit(1);
 					break;
 				case ToastDismissalReason.UserCanceled:
-					outputText = "The user dismissed the notification.";
+					//				var d12 = DismissalActions.Hidden;
+					WriteLine("The user dismissed the notification.");
+					Exit(2);
 					break;
 				case ToastDismissalReason.TimedOut:
-					outputText = "The notification has timed out.";
+					//					var d2 = DismissalActions.Timeout;
+					WriteLine("The notification has timed out.");
+					Exit(3);
 					break;
 			}
-
-			Console.WriteLine(outputText);
 		}
 
 		internal void Failed(ToastNotification sender, ToastFailedEventArgs e)
 		{
-			Console.WriteLine("The notification encountered an error.");
+			WriteLine("The notification encountered an error.");
+			Exit(-1);
 		}
 	}
 }
